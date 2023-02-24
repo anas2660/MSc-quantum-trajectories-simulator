34c80849dbe8ae75e30d9b929ad156478feb8864
A I <candfa2660@gmail.com>
2023-02-24 08:27:19 +0100

    S_k coefficient generator.

--- src/main.rs
+++ src/main.rs
@@ -86,6 +86,27 @@ struct QubitSystem {
     dZ: Complex,
 }
 
+
+struct SGenerator {
+    v: u64,
+    index: u32
+}
+
+impl SGenerator {
+    fn new(rng: &mut ThreadRng) -> Self {
+        Self { v: rng.gen(), index: 0 }
+    }
+
+    fn gen(&mut self, rng: &mut ThreadRng) -> i32 {
+        let result = (((self.v >> self.index) & 1) << 1) as i32 - 1;
+        self.index += 1;
+        if self.index > 64 {
+            *self = SGenerator::new(rng);
+        }
+        result
+    }
+}
+
 #[inline]
 fn commutator(a: &Operator, b: &Operator) -> Operator {
     &(a * b) - &(b * a)
